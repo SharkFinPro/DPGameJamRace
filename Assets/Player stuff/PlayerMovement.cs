ac119ae67ea6bfa32969f7039077a3a003f4d128
Alex Martin <sharkfinpro@hotmail.com>
2021-11-02 14:54:33 -0700

    player updates
     - better player movement
     - jumping

--- Assets/Player stuff/PlayerMovement.cs
+++ Assets/Player stuff/PlayerMovement.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public BoxCollider2D bc;
+
+    public float speed;
+    public float maxSpeed;
+
+    public float jumpHeight;
+    private bool jumping = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,19 +22,30 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("space"))
+
+        if (Input.GetKey("up") && !jumping)
         {
-            rb.AddForce(new Vector2(0f, 500f));
+            rb.AddForce(new Vector2(0f, jumpHeight));
+            jumping = true;
         }
 
-        if (Input.GetKeyDown("right"))
+        if (Input.GetKey("right"))
         {
-            rb.AddForce(new Vector2(150f, 0f));
+            rb.AddForce(new Vector2(speed, 0f));
         }
 
-        if (Input.GetKeyDown("left"))
+        if (Input.GetKey("left"))
         {
-            rb.AddForce(new Vector2(-150f, 0f));
+            rb.AddForce(new Vector2(-speed, 0f));
+        }
+
+        float x = Mathf.Clamp(rb.velocity.x, -maxSpeed, maxSpeed);
+        rb.velocity = new Vector2(x, rb.velocity.y);
+
+        LayerMask mask = LayerMask.GetMask("Platforms");
+        if (bc.IsTouchingLayers(mask))
+        {
+            jumping = false;
         }
     }
 }
