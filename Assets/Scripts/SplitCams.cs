686b80ac82f26d3773d08db0d9d9a29ba6bc8bf7
Alex Martin <sharkfinpro@hotmail.com>
2021-11-04 12:59:39 -0700

    camera update faster

--- Assets/Scripts/SplitCams.cs
+++ Assets/Scripts/SplitCams.cs
@@ -19,12 +19,13 @@ public class SplitCams : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         Vector3 NextCamera1Pos = new Vector3(Camera1.transform.position.x, Player1.transform.position.y + CamOffset, CamDistance);
         Vector3 NextCamera2Pos = new Vector3(Camera2.transform.position.x, Player2.transform.position.y + CamOffset, CamDistance);
         Camera1.transform.position = Vector3.Lerp(Camera1.transform.position, NextCamera1Pos, Time.deltaTime * CamMoveSpeed);
         Camera2.transform.position = Vector3.Lerp(Camera2.transform.position, NextCamera2Pos, Time.deltaTime * CamMoveSpeed);
+
         //Camera1.transform.position = Vector3.SmoothDamp(new Vector3(Camera1.transform.position.x, Camera1.transform.position.y, -10f), NextCamera1Pos, ref velocity, CamMoveSpeed);
         //Camera2.transform.position = Vector3.SmoothDamp(new Vector3(Camera2.transform.position.x, Camera2.transform.position.y, -10f), NextCamera2Pos, ref velocity, CamMoveSpeed);
     }
